96d91de516e73d0ad5487dca765d94b4d43855b4
drogon <abhiramajithr@gmail.com>
2024-06-28 17:01:08 +0530

    fina-commit

--- app.py
+++ app.py
@@ -6,36 +6,42 @@ from webdriver_manager.chrome import ChromeDriverManager
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-chrome_options=Options()
+
+chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--remote-debugging-port=9222')
 
-def scraper(url):
-    driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
-    driver.get(url)
-    header = driver.find_element(By.TAG_NAME, "h1").text
-    return header
-
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 @app.route('/', methods=['POST'])
 def process_request():
     try:
-        # Get user input from POST data
         user_input = request.json['user_input']
-
-        # Process user input to get final result
         result = scraper(user_input)
-
+        
         if result is not None:
-            # Prepare JSON response
             response = {"result": result}
         else:
             response = {"error": "Failed to scrape the webpage"}
-
+            
         return jsonify(response)
     except Exception as e:
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
+def scraper(url):
+    try:
+        driver.get(url)
+        header = driver.find_element(By.TAG_NAME, "h1").text
+        return header
+    except Exception as e:
+        return None
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    driver.quit()
+    return 'Driver shutdown successfully'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8001)
--- dockerfile
+++ dockerfile
@@ -1,14 +1,22 @@
-FROM python:3.12
+FROM python:3.12-slim
 
 WORKDIR /app
 
 COPY . /app
 
-RUN pip install --trusted-host pypi.python.org -r requirements.txt
+RUN apt-get update && apt-get install -y \
+    wget \
+    unzip \
+    curl \
+    gnupg \
+    ca-certificates
 
-RUN apt-get update && apt-get install -y wget unzip &&\
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&\
+RUN apt-get update && apt-get install -y wget unzip && \
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && \
+    apt-get install -y ./google-chrome-stable_current_amd64.deb && \
     rm google-chrome-stable_current_amd64.deb && \
     apt-get clean
 
-CMD ["python","app.py"]
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
+CMD gunicorn app:app --bind 0.0.0.0:8001 --timeout 120
