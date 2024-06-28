31571fb8a804eb3d57ed9ec2f179c437b610db20
drogon <abhiramajithr@gmail.com>
2024-06-28 13:29:05 +0530

    final commit

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+chrome_options=Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 def scraper(url):
-    driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
     driver.get(url)
     header = driver.find_element(By.TAG_NAME, "h1").text
     return header
